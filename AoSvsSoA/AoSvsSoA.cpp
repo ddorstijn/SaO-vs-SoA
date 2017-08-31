18f7ede16f20b0a3d3030689139af0ff2a20c988
Danny Dorstijn <danny.dorstijn@hva.nl>
2017-08-31 10:51:58 +0200

    Fixed memory leak

--- AoSvsSoA/AoSvsSoA.cpp
+++ AoSvsSoA/AoSvsSoA.cpp
@@ -1,31 +1,52 @@
 #include "stdafx.h"
 
-#include <ctime>
-#include <iostream>
 #include <vector>
 
-int main() {
+#define NUM_ITEMS 500000000
+struct Vector3 {
+public:	
+	Vector3() : x(0.0f), y(0.0f), z(0.0f) {}
 
+	float x, y, z;
+
+	Vector3& operator+=(const Vector3& v)
+	{
+		this->x += v.x;
+		this->y += v.y;
+		this->z += v.z;
+
+		return *this;
+	}
+	Vector3 operator+(const Vector3& v) const
+	{
+		auto v_new = *this;
+		v_new += v;
+		return v_new;
+	}
+};
+
+struct SoA {
+	SoA() : pos(NUM_ITEMS), vel(NUM_ITEMS) {}
+
+	std::vector<Vector3> pos, vel;
+};
+
+struct AoS {
+	AoS() {}
+	Vector3 pos, vel;
+};
+
+int main()
+{
+	std::vector<AoS> *entities = new std::vector<AoS>[NUM_ITEMS];
+	for (int i = 0; i < entities->size(); i++) {
+		entities->at(i).pos += entities->at(i).vel;
+	}
+
+	SoA entityManager;
+	for (int i = 0; i < NUM_ITEMS; i++) {
+		entityManager.pos[i] += entityManager.vel[i];
+	}
 
 	return 0;
 }
-
-struct Vector3 {
-	public:
-		Vector3() : x(0.0f), y(0.0f), z(0.0f) {}
-
-		float x, y, z;
-};
-
-class SoA {
-public:
-	SoA() {}
-
-	Vector3 pos, vel;
-};
-
-class AoS {
-	AoS() {}
-
-	Vector3 pos, vel;
-};
